01ca536827d35df811cc8ed026f25308320e90b8
kanerla <kanervalaurajohanna@gmail.com>
2019-03-06 19:32:43 +0200

    First version of buttons

--- core/src/fi/tuni/tiko/MainMenuScreen.java
+++ core/src/fi/tuni/tiko/MainMenuScreen.java
@@ -1,19 +1,72 @@
 package fi.tuni.tiko;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
-public abstract class MainMenuScreen implements Screen {
+public abstract class MainMenuScreen extends ApplicationAdapter implements Screen {
     RaccoonRoll game;
     SpriteBatch batch;
     OrthographicCamera worldCamera;
     OrthographicCamera textCamera;
+    Skin skin;
+    TextButton play;
+    TextButton options;
+    Stage stage;
 
     public MainMenuScreen(RaccoonRoll game) {
         this.game = game;
         batch = game.getBatch();
         worldCamera = game.getWorldCamera();
         textCamera = game.getTextCamera();
+
+        skin = new Skin (Gdx.files.internal("comic-ui.json"));
+        play = new TextButton("Play", skin);
+        options = new TextButton("options", skin);
+        stage = new Stage();
+
+    }
+
+    @Override
+    public void create() {
+        int buttonOffset = 20;
+
+        Gdx.input.setInputProcessor(stage);
+
+        play.setPosition(Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8, Gdx.graphics.getHeight() / 2);
+        play.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Play button clicked");
+                game.setScreen(new Level1Screen(game));
+            }
+        });
+        stage.addActor(play);
+
+        options.setPosition(Gdx.graphics.getWidth() / 2 - Gdx.graphics.getWidth() / 8, Gdx.graphics.getHeight() / 2 - (options.getHeight() + buttonOffset));
+        options.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Here we have options");
+            }
+        });
+        stage.addActor(options);
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 }
