db3ddea450e4fb445dad5c3ec4a3cd4ff5d10ad8
HeikkiKangas <heikki.kangas@cs.tamk.fi>
2019-03-07 18:08:11 +0200

    Refactored Player to separate class

--- core/src/fi/tuni/tiko/MazeScreen.java
+++ core/src/fi/tuni/tiko/MazeScreen.java
@@ -1,16 +1,11 @@
 package fi.tuni.tiko;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
@@ -28,8 +23,6 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
@@ -42,7 +35,6 @@ public class MazeScreen implements Screen {
     OrthographicCamera worldCamera;
     OrthographicCamera textCamera;
     World world;
-    Body playerBody;
     Box2DDebugRenderer debugRenderer;
     TiledMap tiledMap;
     TiledMapRenderer tiledMapRenderer;
@@ -51,12 +43,10 @@ public class MazeScreen implements Screen {
     float tiledMapWidth;
     float tileSize = 16f;
     ArrayList<Rectangle> goodObjectsRectangles;
+    Player player;
 
     // Refactor attributes below this line to separate player class
-    float playerRotation;
-    float playerRadius;
-    Animation<TextureRegion> playerAnimation;
-    float statetime;
+
 
 
     public MazeScreen(RaccoonRoll game, String levelName) {
@@ -66,11 +56,11 @@ public class MazeScreen implements Screen {
         textCamera = game.getTextCamera();
 
         world = new World(new Vector2(0, 0), true);
+        player = new Player(game);
 
         debugRenderer = new Box2DDebugRenderer();
 
-        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("graphics/player/roll_animation/racc_roll.txt"));
-        playerAnimation = new Animation<TextureRegion>(1 / 30f, atlas.findRegions("racc_roll"));
+
 
         TmxMapLoader.Parameters parameters = new TmxMapLoader.Parameters();
         parameters.textureMinFilter = Texture.TextureFilter.Nearest;
@@ -82,24 +72,23 @@ public class MazeScreen implements Screen {
         tiledMapWidth = mapProps.get("width", Integer.class) * tileSize * game.getScale();
         tiledMapHeight = mapProps.get("height", Integer.class) * tileSize * game.getScale();
 
-        MapLayer startPosLayer = tiledMap.getLayers().get("startpos");
-        MapObject startPos = startPosLayer.getObjects().get(0);
-        float startPosX = startPos.getProperties().get("x", Float.class);
-        float startPosY = startPos.getProperties().get("y", Float.class);
-
-        playerBody = world.createBody(getPlayerBodyDef(startPosX, startPosY));
-        playerBody.createFixture(getPlayerFixtureDef());
-
+        player.createPlayerBody(world, getPlayerStartPos());
         goodObjectsRectangles = getGoodRectangles();
 
         createWalls();
-        playerRotation = 90;
-        playerRadius = 12 * game.getScale();
     }
 
     public void clearScreen() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    public Vector2 getPlayerStartPos() {
+        MapLayer startPosLayer = tiledMap.getLayers().get("startpos");
+        MapObject startPos = startPosLayer.getObjects().get(0);
+        float startPosX = startPos.getProperties().get("x", Float.class);
+        float startPosY = startPos.getProperties().get("y", Float.class);
+        return new Vector2(startPosX, startPosY);
     }
 
     @Override
@@ -110,66 +99,22 @@ public class MazeScreen implements Screen {
     @Override
     public void render(float delta) {
         clearScreen();
-        movePlayer(delta);
+        player.movePlayer(delta);
         updateCameraPosition();
         checkGoodObjectOverlaps();
-        Vector2 playerVelocity = playerBody.getLinearVelocity();
-        if (playerVelocity.x != 0 || playerVelocity.y != 0) {
-            playerRotation = playerBody.getLinearVelocity().angle();
-            statetime += delta * Math.max(Math.abs(playerVelocity.x), Math.abs(playerVelocity.y));
-        }
+
         tiledMapRenderer.setView(worldCamera);
         tiledMapRenderer.render();
         //debugRenderer.render(world, worldCamera.combined);
         batch.setProjectionMatrix(worldCamera.combined);
         batch.begin();
-        batch.draw(
-                playerAnimation.getKeyFrame(statetime, true),
-                playerBody.getPosition().x - playerRadius,
-                playerBody.getPosition().y - playerRadius,
-                playerRadius,
-                playerRadius,
-                playerRadius * 2,
-                playerRadius * 2,
-                1.0f,
-                1.0f,
-                playerRotation - 90);
+        player.draw(batch, delta);
         batch.end();
         world.step(1 / 60f, 6, 2);
     }
 
-    private void checkGoodObjectOverlaps() {
-        ArrayList<Rectangle> rectanglesToRemove = new ArrayList<Rectangle>();
-        float playerX = playerBody.getPosition().x;
-        float playerY = playerBody.getPosition().y;
-        // Vector2 playerPos = playerBody.getWorldCenter();
-        Circle playerCircle = new Circle(playerX, playerY, playerRadius);
-
-        for (Rectangle rectangle : goodObjectsRectangles) {
-            if (Intersector.overlaps(playerCircle, rectangle)) {
-                rectanglesToRemove.add(rectangle);
-                clearTile(
-                        (TiledMapTileLayer) tiledMap.getLayers().get("good_tiles"),
-                        getRectangleTileIndex(rectangle)
-                );
-            }
-        }
-        goodObjectsRectangles.removeAll(rectanglesToRemove);
-    }
-
-    private Vector2 getRectangleTileIndex(Rectangle rectangle) {
-        float scale = game.getScale();
-        return new Vector2(rectangle.x / scale / tileSize, rectangle.y / scale / tileSize);
-    }
-
-    private void clearTile(TiledMapTileLayer tileLayer, Vector2 tileIndex) {
-        int x = (int) tileIndex.x;
-        int y = (int) tileIndex.y;
-        tileLayer.setCell(x, y, null);
-    }
-
     private void updateCameraPosition() {
-        Vector2 playerPos = playerBody.getPosition();
+        Vector2 playerPos = player.getPosition();
         float playerX = playerPos.x;
         float playerY = playerPos.y;
         float worldWidth = game.getWORLD_WIDTH();
@@ -197,65 +142,35 @@ public class MazeScreen implements Screen {
         worldCamera.update();
     }
 
-    private void movePlayer(float deltatime) {
-        float x = 0;
-        float y = 0;
+    private void checkGoodObjectOverlaps() {
+        ArrayList<Rectangle> rectanglesToRemove = new ArrayList<Rectangle>();
+        Vector2 playerPos = player.getPosition();
+        Circle playerCircle = new Circle(playerPos.x, playerPos.y, player.getBodyRadius());
 
-        if (Gdx.app.getType() == Application.ApplicationType.Desktop) {
-            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-                x = 150f * deltatime;
-            } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-                x = -150f * deltatime;
-            }
-            if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-                y = 150f * deltatime;
-            } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-                y = -150f * deltatime;
-            }
-        } else if (Gdx.app.getType() == Application.ApplicationType.Android) {
-            x = Gdx.input.getAccelerometerY() * 25 * deltatime;
-            y = Gdx.input.getAccelerometerX() * 25 * deltatime;
-            if (x < 0.2 && x > -0.2) {
-                x = 0;
-            }
-            if (y < 0.2 && y > -0.2) {
-                y = 0;
-            }
-            if (y > 0) {
-                y = -y;
-            } else {
-                y = Math.abs(y);
+        for (Rectangle rectangle : goodObjectsRectangles) {
+            if (Intersector.overlaps(playerCircle, rectangle)) {
+                rectanglesToRemove.add(rectangle);
+                clearTile(
+                        (TiledMapTileLayer) tiledMap.getLayers().get("good_tiles"),
+                        getRectangleTileIndex(rectangle)
+                );
             }
         }
-        /*
-        playerBody.applyForceToCenter(
-                new Vector2(x, y),
-                true);
-        */
-        playerBody.setLinearVelocity(x, y);
+        goodObjectsRectangles.removeAll(rectanglesToRemove);
     }
 
-    private BodyDef getPlayerBodyDef(float x, float y) {
+    private Vector2 getRectangleTileIndex(Rectangle rectangle) {
         float scale = game.getScale();
-        BodyDef playerBodyDef = new BodyDef();
-        playerBodyDef.type = BodyDef.BodyType.DynamicBody;
-        playerBodyDef.position.set(x * scale, y * scale);
-        return playerBodyDef;
+        return new Vector2(rectangle.x / scale / tileSize, rectangle.y / scale / tileSize);
     }
 
-    private FixtureDef getPlayerFixtureDef() {
-        FixtureDef playerFixtureDef = new FixtureDef();
-        playerFixtureDef.density = 0.1f;
-        playerFixtureDef.restitution = 0.1f;
-        playerFixtureDef.friction = 0.75f;
-
-        CircleShape playerCircle = new CircleShape();
-        playerCircle.setRadius(12 * game.getScale());
-
-        playerFixtureDef.shape = playerCircle;
-
-        return playerFixtureDef;
+    private void clearTile(TiledMapTileLayer tileLayer, Vector2 tileIndex) {
+        int x = (int) tileIndex.x;
+        int y = (int) tileIndex.y;
+        tileLayer.setCell(x, y, null);
     }
+
+
 
     private void createWalls() {
         ArrayList<Rectangle> wallRectangles = getWallRectangles();
--- core/src/fi/tuni/tiko/Player.java
+++ core/src/fi/tuni/tiko/Player.java
@@ -1,5 +1,128 @@
 package fi.tuni.tiko;
 
-public class Player {
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
 
+public class Player {
+    Body playerBody;
+    float playerRotation;
+    float playerRadius;
+    Animation<TextureRegion> playerAnimation;
+    float statetime;
+    RaccoonRoll game;
+
+    public Player(RaccoonRoll game) {
+        this.game = game;
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("graphics/player/roll_animation/racc_roll.txt"));
+        playerAnimation = new Animation<TextureRegion>(
+                1 / 30f,
+                atlas.findRegions("racc_roll")
+        );
+        playerRotation = 90;
+        playerRadius = 12 * game.getScale();
+    }
+
+    public void draw(SpriteBatch batch, float delta) {
+        Vector2 playerVelocity = playerBody.getLinearVelocity();
+        if (playerVelocity.x != 0 || playerVelocity.y != 0) {
+            playerRotation = playerBody.getLinearVelocity().angle();
+            statetime += delta * Math.max(Math.abs(playerVelocity.x), Math.abs(playerVelocity.y));
+        }
+
+        batch.draw(
+                playerAnimation.getKeyFrame(statetime, true),
+                playerBody.getPosition().x - playerRadius,
+                playerBody.getPosition().y - playerRadius,
+                playerRadius,
+                playerRadius,
+                playerRadius * 2,
+                playerRadius * 2,
+                1.0f,
+                1.0f,
+                playerRotation - 90);
+    }
+
+    public void createPlayerBody(World world, Vector2 startPos) {
+        playerBody = world.createBody(getPlayerBodyDef(startPos.x, startPos.y));
+        playerBody.createFixture(getPlayerFixtureDef());
+    }
+
+    public Vector2 getPosition() {
+        return playerBody.getPosition();
+    }
+
+    public float getBodyRadius() {
+        return playerRadius;
+    }
+
+    public void movePlayer(float deltatime) {
+        float x = 0;
+        float y = 0;
+
+        if (Gdx.app.getType() == Application.ApplicationType.Desktop) {
+            if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+                x = 150f * deltatime;
+            } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+                x = -150f * deltatime;
+            }
+            if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+                y = 150f * deltatime;
+            } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                y = -150f * deltatime;
+            }
+        } else if (Gdx.app.getType() == Application.ApplicationType.Android) {
+            x = Gdx.input.getAccelerometerY() * 25 * deltatime;
+            y = Gdx.input.getAccelerometerX() * 25 * deltatime;
+            if (x < 0.2 && x > -0.2) {
+                x = 0;
+            }
+            if (y < 0.2 && y > -0.2) {
+                y = 0;
+            }
+            if (y > 0) {
+                y = -y;
+            } else {
+                y = Math.abs(y);
+            }
+        }
+        /*
+        playerBody.applyForceToCenter(
+                new Vector2(x, y),
+                true);
+        */
+        playerBody.setLinearVelocity(x, y);
+    }
+
+    private BodyDef getPlayerBodyDef(float x, float y) {
+        float scale = game.getScale();
+        BodyDef playerBodyDef = new BodyDef();
+        playerBodyDef.type = BodyDef.BodyType.DynamicBody;
+        playerBodyDef.position.set(x * scale, y * scale);
+        return playerBodyDef;
+    }
+
+    private FixtureDef getPlayerFixtureDef() {
+        FixtureDef playerFixtureDef = new FixtureDef();
+        playerFixtureDef.density = 0.1f;
+        playerFixtureDef.restitution = 0.1f;
+        playerFixtureDef.friction = 0.75f;
+
+        CircleShape playerCircle = new CircleShape();
+        playerCircle.setRadius(12 * game.getScale());
+
+        playerFixtureDef.shape = playerCircle;
+
+        return playerFixtureDef;
+    }
 }
