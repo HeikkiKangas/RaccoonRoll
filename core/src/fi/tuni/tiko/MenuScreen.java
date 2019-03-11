c0749243347cf6baf8307f3ecfdc39faac01ef47
kanerla <kanervalaurajohanna@gmail.com>
2019-03-11 11:24:31 +0200

    Logo added

--- core/src/fi/tuni/tiko/MenuScreen.java
+++ core/src/fi/tuni/tiko/MenuScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
@@ -34,12 +35,14 @@ public class MenuScreen extends ApplicationAdapter implements Screen {
     I18NBundle menuBundle;
     //Label title;
     float buttonHeight;
+    Texture title;
 
     public MenuScreen(RaccoonRoll game) {
         this.game = game;
         batch = game.getBatch();
         worldCamera = game.getWorldCamera();
         textCamera = game.getTextCamera();
+        title = new Texture("graphics/mainmenu/Logo1.png");
 
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
@@ -121,6 +124,11 @@ public class MenuScreen extends ApplicationAdapter implements Screen {
 
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+
+        batch.setProjectionMatrix(textCamera.combined);
+        batch.begin();
+        batch.draw(title, 10, 10);
+        batch.end();
     }
 
     @Override
