0478b14f431506a042ea48dfc0347860ae1233e9
kanerla <kanervalaurajohanna@gmail.com>
2019-03-21 09:59:58 +0200

    Map Screen created

---
+++ core/src/fi/tuni/tiko2d/MapScreen.java
@@ -0,0 +1,23 @@
+package fi.tuni.tiko2d;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Screen;
+
+//THIS IS WHERE LEVEL MAP WILL BE
+
+public class MapScreen extends ApplicationAdapter implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
